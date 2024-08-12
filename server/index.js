435c2588b3fcc2f370a25dfafd4288fd403256b3
Mahmoud Husam <mahayesh7@gmail.com>
2024-08-12 17:34:45 +0300

    Added .gitignore for server, updated server/index.js, and made changes in client

--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
-const __filname = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filname);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -33,7 +33,6 @@ app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -42,30 +41,28 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
-
 const upload = multer({ storage });
 
-/* ROUTES WITH FILES*/
-
-app.post("auth/register", upload.single("picture"), register);
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
-/* ROUTES*/
+
+/* ROUTES */
 app.use("/auth", authRoutes);
-app.use("/user", userRoutes);
+app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-/*MONGOOSE SETUP */
+/* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
-
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME*/
+    /* ADD DATA ONE TIME */
     // User.insertMany(users);
     // Post.insertMany(posts);
   })
